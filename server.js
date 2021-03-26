12a8937478ddcfe0402a56c140386cf139571d56
Flavio Gragnolati <flavio.gragnolati@gmail.com>
2021-03-26 13:22:36 -0300

    clean up server.js

--- server.js
+++ server.js
@@ -6,32 +6,33 @@ const morgan = require('morgan');
 const routes = require('./routes/index');
 const passport = require('passport');
 
-require('./config/passport')(passport); //pasamos la configuracion al obj global de passport
+require('./config/passport')(passport); //passport config
 
 const server = express();
 
 /**
- * SETUP GENERAL
+ * GENERAL SETUP 
  */
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(morgan('dev')); //para logear a la terminal
+
+server.use(morgan('dev')); //temrinal logs
+
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); //Linea agregada para probar por que no axios.delete no recibe ninguna respuesta
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
 });
+
 server.use(cors({ origin: true, credentials: true }));
 
-//server.use(express.session({ secret: 'barto', maxAge: null }))
-server.use(passport.initialize()); //*Inicializamos el passport
-//server.use(passport.session()); // Sesión persistente
+server.use(passport.initialize()); //start up passport
 
 server.use('/', routes);
 // Error catching endware.
